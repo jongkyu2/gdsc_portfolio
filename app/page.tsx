d59a6109167d76c326d0ecb5280a2cfa991dd346
jongkyu2 <thinkfire@naver.com>
2024-04-30 23:15:04 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     
   <main>
-    <section className="h-screen flex flex-col justify-conter">
+    <section className="h-screen flex flex-col justify-center">
       <h1 className="text-5xl font-bold">누구누구의 사이트!!! </h1>
       <div>
         <p>백엔드 엔지니어</p>
