f48f7158f0e1d8cb3d04ef4a971c2369eb886a18
jongkyu2 <thinkfire@naver.com>
2024-04-30 23:10:58 +0900

    comit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,19 @@ export default function Home() {
   return (
     
   <main>
-      <h1>누구누구의 사이트!!! </h1>
+    <section className="h-screen flex flex-col justify-conter">
+      <h1 className="text-5xl font-bold">누구누구의 사이트!!! </h1>
+      <div>
+        <p>백엔드 엔지니어</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>naver.com</li>
+        </ul>
+      </div>
+    </section>
+    
     </main>
   );
 }
